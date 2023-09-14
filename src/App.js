10b4a699073cf82604b37c7f39c745e31878a8d8
diegofeva <diegofeva@gmail.com>
2023-09-14 22:36:05 +0200

    Into + Setup

--- src/App.js
+++ src/App.js
@@ -1,69 +1,12 @@
 
+
+import Directory from './components/directory/directory.component';
+
 const App = () => {
+
   return (
-    <div clasName="categorier-container">
 
-      <div className="category-contanier">
-        
-        {/*<img/>*/}
-        <div className="category-body-container">
-
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        
-        </div>
-
-      </div>
-
-      <div className="category-contanier">
-        
-        {/*<img/>*/}
-        <div className="category-body-container">
-
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        
-        </div>
-
-      </div>
-
-      <div className="category-contanier">
-        
-        {/*<img/>*/}
-        <div className="category-body-container">
-
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        
-        </div>
-
-      </div>    
-
-      <div className="category-contanier">
-        
-        {/*<img/>*/}
-        <div className="category-body-container">
-
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        
-        </div>
-
-      </div>
-
-      <div className="category-contanier">
-        
-        {/*<img/>*/}
-        <div className="category-body-container">
-
-          <h2>Mans</h2>
-          <p>Shop Now</p>
-        
-        </div>
-
-      </div>        
-
-    </div>
+    <Directory/>
     
   );
 }
